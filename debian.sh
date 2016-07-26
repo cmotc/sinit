2f26a52b41d82fed2cf11fb2e71084ee1cc41d3b
idk <idk@devuan.local>
2016-07-25 20:10:05 -0400

    Mon Jul 25 20:10:05 EDT 2016

--- debian.sh
+++ debian.sh
@@ -34,7 +34,7 @@ cp $SOURCEBINPATH/ $DEBFOLDERNAME -r
 cd $DEBFOLDERNAME
 
 # Create the packaging skeleton (debian/*)
-dh_make -s --indep --createorig
+dh_make --indep --createorig
 echo "$CONTROL_FILE" > debian/control
 
 # Remove make calls
